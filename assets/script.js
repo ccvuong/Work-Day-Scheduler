ce8003ea6433c23070db84979ef4878146bb7137
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-03-24 22:59:03 -0700

    updated files

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,48 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+$(init);
+
+function init() {
+  // current day in header area
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  // color our time blocks and start interval to re-color every minute
+  timeOfDayColors();
+  setInterval(timeOfDayColors, 60000);
+
+  // looking out for user input per time of day
+  $(".time-of-day").each(function () {
+    var userTask = $(this).attr("id");
+    $("#" + userTask + " #inputArea").text(localStorage.getItem(moment().format("DDDYYYY") + userTask));
   });
+
+  // looking out for user saving via click with saveGame function below
+  $(".saveBtn").on("click", saveGame);
+}
+
+function timeOfDayColors() {
+  // for each time of day 
+  $(".time-of-day").each(function () {
+
+    var theHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var presentHour = parseInt(moment().format("H"));
+
+    // clears out events on the planner
+    $(this).removeClass("past present future");
+    // determines the color of event on planner with CSS
+    if (theHour < presentHour) {
+      $(this).addClass("past");
+
+    } else if (theHour > presentHour) {
+      $(this).addClass("future");
+
+    } else {
+      $(this).addClass("present");
+    }
+
+  });
+};
+
+function saveGame(event) {
+  var userAddEvent = $(this).parent().attr("id");
+  // saves user input into localstorage
+  localStorage.setItem(moment().format("DDDYYYY") + userAddEvent, $("#" + userAddEvent + " #inputArea").val());
+};
